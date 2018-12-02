edda3a041d8aa275c32891332759561ac31c4568
Henri Sivonen <hsivonen@hsivonen.fi>
2018-12-02 16:58:35 +0200

    Actually implement the crate.

--- src/lib.rs
+++ src/lib.rs
@@ -7,22 +7,44 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+#![doc(html_root_url = "https://docs.rs/codepage/0.1.0")]
+
+//! Mapping between Windows [code page identifiers][1] and
+//! [encoding_rs][2] `Encoding`s.
+//!
+//! [1]: https://docs.microsoft.com/en-us/windows/desktop/intl/code-page-identifiers
+//! [2]: https://crates.io/crates/encoding_rs/
+
 extern crate encoding_rs;
+
+#[cfg(test)]
+mod tests;
 
 use encoding_rs::*;
 
-static CODE_PAGES_SORTED: [u16; 2] = [1252, 65001];
-
-static ENCODINGS_IN_CODE_PAGE_SORT: [&'static Encoding; 2] = [&WINDOWS_1252_INIT, &UTF_8_INIT];
-
+/// Maps a Windows code page identifier to an encoding_rs `Encoding`
+/// (or `None` if there is no appropriate mapping).
+///
+/// In some cases, multiple code page identifiers maps to a single
+/// `Encoding`. For example, `28591` for ISO-8859-1 maps to the
+/// windows-1252 encoding, since the two are unified in the Web
+/// Platform. The EUC family of CJK encodings has multiple code
+/// page identifiers. For example, EUC-KR has `949`, `20949` and
+/// `51949`. (At present, x-mac-korean, `10003`, maps to `None`,
+/// though.)
+///
+/// Code page identifiers whose corresponding labels would map to
+/// the replacement encoding also map to the replacement encoding
+/// here.
 pub fn to_encoding(code_page: u16) -> Option<&'static Encoding> {
-    if let Ok(i) = CODE_PAGES_SORTED.binary_search(&code_page) {
-        Some(ENCODINGS_IN_CODE_PAGE_SORT[i])
-    } else {
-        None
-    }
+    CODE_PAGES
+        .iter()
+        .position(|&x| x == code_page)
+        .map(|i| ENCODINGS[i])
 }
 
+/// Like `to_encoding`, except returns `None` when `to_encoding`
+/// would return `Some(REPLACEMENT)`.
 pub fn to_encoding_no_replacement(code_page: u16) -> Option<&'static Encoding> {
     let opt_encoding = to_encoding(code_page);
     if opt_encoding == Some(REPLACEMENT) {
@@ -32,21 +54,84 @@ pub fn to_encoding_no_replacement(code_page: u16) -> Option<&'static Encoding> {
     }
 }
 
+/// Returns the preferred code page identifier for an `Encoding`.
+///
+/// Returns `None` for replacement and x-user-defined.
 pub fn from_encoding(encoding: &'static Encoding) -> Option<u16> {
     if encoding == REPLACEMENT {
         None
     } else {
-        ENCODINGS_IN_CODE_PAGE_SORT
+        ENCODINGS
             .iter()
             .position(|&x| x == encoding)
-            .map(|i| CODE_PAGES_SORTED[i])
+            .map(|i| CODE_PAGES[i])
     }
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// BEGIN GENERATED CODE. PLEASE DO NOT EDIT.
+// Instead, please regenerate using generate-encoding-data.py
+
+/// Supported code page numbers in estimated order of usage frequency
+static CODE_PAGES: [u16; 51] = [
+    65001, 1200, 1252, 1251, 936, 932, 949, 1250, 1256, 1254, 950, 874, 1255, 1253, 1257, 1258,
+    20932, 28592, 28605, 28597, 20866, 54936, 28595, 38598, 28594, 28596, 50221, 21866, 28603,
+    28593, 1201, 866, 28600, 28598, 10000, 10017, 28604, 28606, 951, 20936, 20949, 28591, 28599,
+    28601, 50220, 50222, 50225, 50227, 51936, 51949, 52936,
+];
+
+/// Encodings corresponding to the code page numbers in the same order
+static ENCODINGS: [&'static Encoding; 51] = [
+    &UTF_8_INIT,
+    &UTF_16LE_INIT,
+    &WINDOWS_1252_INIT,
+    &WINDOWS_1251_INIT,
+    &GBK_INIT,
+    &SHIFT_JIS_INIT,
+    &EUC_KR_INIT,
+    &WINDOWS_1250_INIT,
+    &WINDOWS_1256_INIT,
+    &WINDOWS_1254_INIT,
+    &BIG5_INIT,
+    &WINDOWS_874_INIT,
+    &WINDOWS_1255_INIT,
+    &WINDOWS_1253_INIT,
+    &WINDOWS_1257_INIT,
+    &WINDOWS_1258_INIT,
+    &EUC_JP_INIT,
+    &ISO_8859_2_INIT,
+    &ISO_8859_15_INIT,
+    &ISO_8859_7_INIT,
+    &KOI8_R_INIT,
+    &GB18030_INIT,
+    &ISO_8859_5_INIT,
+    &ISO_8859_8_I_INIT,
+    &ISO_8859_4_INIT,
+    &ISO_8859_6_INIT,
+    &ISO_2022_JP_INIT,
+    &KOI8_U_INIT,
+    &ISO_8859_13_INIT,
+    &ISO_8859_3_INIT,
+    &UTF_16BE_INIT,
+    &IBM866_INIT,
+    &ISO_8859_10_INIT,
+    &ISO_8859_8_INIT,
+    &MACINTOSH_INIT,
+    &X_MAC_CYRILLIC_INIT,
+    &ISO_8859_14_INIT,
+    &ISO_8859_16_INIT,
+    &BIG5_INIT,
+    &GBK_INIT,
+    &EUC_KR_INIT,
+    &WINDOWS_1252_INIT,
+    &WINDOWS_1254_INIT,
+    &WINDOWS_874_INIT,
+    &ISO_2022_JP_INIT,
+    &ISO_2022_JP_INIT,
+    &REPLACEMENT_INIT,
+    &REPLACEMENT_INIT,
+    &GBK_INIT,
+    &EUC_KR_INIT,
+    &REPLACEMENT_INIT,
+];
+
+// END GENERATED CODE
---
+++ src/tests.rs
@@ -0,0 +1,119 @@
+// Any copyright to the test code below this comment is dedicated to the
+// Public Domain. http://creativecommons.org/publicdomain/zero/1.0/
+
+use super::*;
+use encoding_rs::*;
+
+#[test]
+fn test_to_encoding_no_replacement() {
+    assert_eq!(to_encoding_no_replacement(50225), None);
+    assert_eq!(to_encoding_no_replacement(50227), None);
+    assert_eq!(to_encoding_no_replacement(52936), None);
+
+    assert_eq!(to_encoding_no_replacement(65001), Some(UTF_8));
+}
+
+// BEGIN GENERATED CODE. PLEASE DO NOT EDIT.
+// Instead, please regenerate using generate-encoding-data.py
+
+#[test]
+fn test_to_encoding() {
+    assert_eq!(to_encoding(0), None);
+
+    assert_eq!(to_encoding(65001), Some(UTF_8));
+    assert_eq!(to_encoding(1200), Some(UTF_16LE));
+    assert_eq!(to_encoding(1252), Some(WINDOWS_1252));
+    assert_eq!(to_encoding(1251), Some(WINDOWS_1251));
+    assert_eq!(to_encoding(936), Some(GBK));
+    assert_eq!(to_encoding(932), Some(SHIFT_JIS));
+    assert_eq!(to_encoding(949), Some(EUC_KR));
+    assert_eq!(to_encoding(1250), Some(WINDOWS_1250));
+    assert_eq!(to_encoding(1256), Some(WINDOWS_1256));
+    assert_eq!(to_encoding(1254), Some(WINDOWS_1254));
+    assert_eq!(to_encoding(950), Some(BIG5));
+    assert_eq!(to_encoding(874), Some(WINDOWS_874));
+    assert_eq!(to_encoding(1255), Some(WINDOWS_1255));
+    assert_eq!(to_encoding(1253), Some(WINDOWS_1253));
+    assert_eq!(to_encoding(1257), Some(WINDOWS_1257));
+    assert_eq!(to_encoding(1258), Some(WINDOWS_1258));
+    assert_eq!(to_encoding(20932), Some(EUC_JP));
+    assert_eq!(to_encoding(28592), Some(ISO_8859_2));
+    assert_eq!(to_encoding(28605), Some(ISO_8859_15));
+    assert_eq!(to_encoding(28597), Some(ISO_8859_7));
+    assert_eq!(to_encoding(20866), Some(KOI8_R));
+    assert_eq!(to_encoding(54936), Some(GB18030));
+    assert_eq!(to_encoding(28595), Some(ISO_8859_5));
+    assert_eq!(to_encoding(38598), Some(ISO_8859_8_I));
+    assert_eq!(to_encoding(28594), Some(ISO_8859_4));
+    assert_eq!(to_encoding(28596), Some(ISO_8859_6));
+    assert_eq!(to_encoding(50221), Some(ISO_2022_JP));
+    assert_eq!(to_encoding(21866), Some(KOI8_U));
+    assert_eq!(to_encoding(28603), Some(ISO_8859_13));
+    assert_eq!(to_encoding(28593), Some(ISO_8859_3));
+    assert_eq!(to_encoding(1201), Some(UTF_16BE));
+    assert_eq!(to_encoding(866), Some(IBM866));
+    assert_eq!(to_encoding(28600), Some(ISO_8859_10));
+    assert_eq!(to_encoding(28598), Some(ISO_8859_8));
+    assert_eq!(to_encoding(10000), Some(MACINTOSH));
+    assert_eq!(to_encoding(10017), Some(X_MAC_CYRILLIC));
+    assert_eq!(to_encoding(28604), Some(ISO_8859_14));
+    assert_eq!(to_encoding(28606), Some(ISO_8859_16));
+    assert_eq!(to_encoding(951), Some(BIG5));
+    assert_eq!(to_encoding(20936), Some(GBK));
+    assert_eq!(to_encoding(20949), Some(EUC_KR));
+    assert_eq!(to_encoding(28591), Some(WINDOWS_1252));
+    assert_eq!(to_encoding(28599), Some(WINDOWS_1254));
+    assert_eq!(to_encoding(28601), Some(WINDOWS_874));
+    assert_eq!(to_encoding(50220), Some(ISO_2022_JP));
+    assert_eq!(to_encoding(50222), Some(ISO_2022_JP));
+    assert_eq!(to_encoding(50225), Some(REPLACEMENT));
+    assert_eq!(to_encoding(50227), Some(REPLACEMENT));
+    assert_eq!(to_encoding(51936), Some(GBK));
+    assert_eq!(to_encoding(51949), Some(EUC_KR));
+    assert_eq!(to_encoding(52936), Some(REPLACEMENT));
+}
+
+#[test]
+fn test_from_encoding() {
+    assert_eq!(from_encoding(BIG5), Some(950));
+    assert_eq!(from_encoding(EUC_JP), Some(20932));
+    assert_eq!(from_encoding(EUC_KR), Some(949));
+    assert_eq!(from_encoding(GBK), Some(936));
+    assert_eq!(from_encoding(IBM866), Some(866));
+    assert_eq!(from_encoding(ISO_2022_JP), Some(50221));
+    assert_eq!(from_encoding(ISO_8859_10), Some(28600));
+    assert_eq!(from_encoding(ISO_8859_13), Some(28603));
+    assert_eq!(from_encoding(ISO_8859_14), Some(28604));
+    assert_eq!(from_encoding(ISO_8859_15), Some(28605));
+    assert_eq!(from_encoding(ISO_8859_16), Some(28606));
+    assert_eq!(from_encoding(ISO_8859_2), Some(28592));
+    assert_eq!(from_encoding(ISO_8859_3), Some(28593));
+    assert_eq!(from_encoding(ISO_8859_4), Some(28594));
+    assert_eq!(from_encoding(ISO_8859_5), Some(28595));
+    assert_eq!(from_encoding(ISO_8859_6), Some(28596));
+    assert_eq!(from_encoding(ISO_8859_7), Some(28597));
+    assert_eq!(from_encoding(ISO_8859_8), Some(28598));
+    assert_eq!(from_encoding(ISO_8859_8_I), Some(38598));
+    assert_eq!(from_encoding(KOI8_R), Some(20866));
+    assert_eq!(from_encoding(KOI8_U), Some(21866));
+    assert_eq!(from_encoding(SHIFT_JIS), Some(932));
+    assert_eq!(from_encoding(UTF_16BE), Some(1201));
+    assert_eq!(from_encoding(UTF_16LE), Some(1200));
+    assert_eq!(from_encoding(UTF_8), Some(65001));
+    assert_eq!(from_encoding(GB18030), Some(54936));
+    assert_eq!(from_encoding(MACINTOSH), Some(10000));
+    assert_eq!(from_encoding(REPLACEMENT), None);
+    assert_eq!(from_encoding(WINDOWS_1250), Some(1250));
+    assert_eq!(from_encoding(WINDOWS_1251), Some(1251));
+    assert_eq!(from_encoding(WINDOWS_1252), Some(1252));
+    assert_eq!(from_encoding(WINDOWS_1253), Some(1253));
+    assert_eq!(from_encoding(WINDOWS_1254), Some(1254));
+    assert_eq!(from_encoding(WINDOWS_1255), Some(1255));
+    assert_eq!(from_encoding(WINDOWS_1256), Some(1256));
+    assert_eq!(from_encoding(WINDOWS_1257), Some(1257));
+    assert_eq!(from_encoding(WINDOWS_1258), Some(1258));
+    assert_eq!(from_encoding(WINDOWS_874), Some(874));
+    assert_eq!(from_encoding(X_MAC_CYRILLIC), Some(10017));
+    assert_eq!(from_encoding(X_USER_DEFINED), None);
+}
+// END GENERATED CODE
